fb5a3ce7022d47e210fadfa9f949b1d6d1858bd9
EssieDyva <st3f1703@gmail.com>
2026-02-12 12:28:53 +0100

    same fix from ServiceMacchina applied

--- src/com/betacom/car/services/ServiceBici.java
+++ src/com/betacom/car/services/ServiceBici.java
@@ -1,4 +1,5 @@
 package com.betacom.car.services;
+
 import java.util.List;
 import java.util.Optional;
 
@@ -15,19 +16,29 @@ public class ServiceBici {
 	private final SQLManager db;
 	private final VeicoliDAO daoV;
 	private final BiciDAO daoBici;
-	
+
 	public ServiceBici() {
 		this.db = new SQLManager();
 		this.daoV = new VeicoliDAO();
 		this.daoBici = new BiciDAO();
 	}
-	
+
 	public void executeQuery() throws Exception {
-		listTable();
-		getAllBici();
-		// getBiciById(null);
-		int idVeicolo = createVeicolo();
-		createBici(idVeicolo);
+		System.out.println("Begin transaction");
+		try {
+			SQLConfiguration.getInstance().setTransaction();
+			// listTable();
+			// getAllBici();
+			// getBiciById(null);
+			int idVeicolo = createVeicolo();
+			createBici(idVeicolo);
+
+			db.commit();
+		} catch (Exception e) {
+			System.err.println("Error found: " + e.getMessage());
+			System.err.println("Execute rollback...");
+			db.rollback();
+		}
 	}
 
 	private void listTable() throws VeicoliException {
@@ -37,7 +48,7 @@ public class ServiceBici {
 
 	private void getAllBici() {
 		System.out.println(">>>getAllBici<<<");
-		
+
 		try {
 			List<Bici> lM = daoBici.findAll();
 			lM.forEach(d -> System.out.println(d));
@@ -46,12 +57,12 @@ public class ServiceBici {
 			System.err.println("Error found " + e.getMessage());
 		}
 	}
-	
+
 	private void getBiciById(Integer id) {
 		System.out.println(">>>getBiciById<<<");
 		try {
 			Optional<Bici> bici = daoBici.findById(id);
-			if(bici.isPresent())
+			if (bici.isPresent())
 				System.out.println(bici.get());
 			else {
 				System.out.println("Bici non trovata: " + id);
@@ -60,31 +71,26 @@ public class ServiceBici {
 			System.err.println("Error found " + e.getMessage());
 		}
 	}
-	
-	private int createVeicolo() throws VeicoliException {
+
+	private int createVeicolo() throws Exception {
 		System.out.println("Insert into veicoli:");
-		System.out.println(SQLConfiguration.getInstance().getQuery("update.veicoli.insert"));
-		
 		int num = 0;
-		
+
 		Veicoli vei = new Veicoli(null, null, null, null, null, null, null, null);
-		
-		try {
-			num = daoV.insert("update.veicoli.insert", vei);
-			System.out.println("Inserimento veicolo: " + num);
-			
-			List<Veicoli> lC = daoV.findAll();
-			lC.forEach(c -> System.out.println(c));
-			return num;
-		} catch (Exception e) {
-			System.err.println("inserimento fallito: " + num);
-		}
+
+		num = daoV.insert("update.veicoli.insert", vei);
+		System.out.println("Inserimento veicolo: " + num);
+
+		Optional<Veicoli> v = daoV.findById(num);
+		if (v.isEmpty()) throw new VeicoliException("Veicolo non trovato");
+		System.out.println(v.get());
+
 		return num;
 	}
-	
+
 	private void createBici(int idVeicolo) throws Exception {
 		System.out.println("insert into Bici*****");
-		Bici bici = new Bici(idVeicolo, 6, 1 , 2, true);
+		Bici bici = new Bici(idVeicolo, 6, 1, 2, true);
 
 		daoBici.insert(bici);
 		System.out.println("Bici creato");
--- src/com/betacom/car/services/ServiceMoto.java
+++ src/com/betacom/car/services/ServiceMoto.java
@@ -1,4 +1,5 @@
 package com.betacom.car.services;
+
 import java.util.List;
 import java.util.Optional;
 
@@ -12,82 +13,87 @@ import com.betacom.car.utils.SQLManager;
 
 public class ServiceMoto {
 
-		private final SQLManager db;
-		private final VeicoliDAO daoV;
-		private final MotoDAO daoMoto;
-		
-		public ServiceMoto() {
-			this.db = new SQLManager();
-			this.daoV = new VeicoliDAO();
-			this.daoMoto = new MotoDAO();
-		}
-		
-		public void executeQuery() throws Exception {
-			listTable();
-			getAllMoto();
+	private final SQLManager db;
+	private final VeicoliDAO daoV;
+	private final MotoDAO daoMoto;
+
+	public ServiceMoto() {
+		this.db = new SQLManager();
+		this.daoV = new VeicoliDAO();
+		this.daoMoto = new MotoDAO();
+	}
+
+	public void executeQuery() throws Exception {
+		System.out.println("Begin transaction");
+		try {
+			SQLConfiguration.getInstance().setTransaction();
+			// listTable();
+			// getAllMoto();
 			// getMotoById(null);
 			int idVeicolo = createVeicolo();
 			createMoto(idVeicolo);
-		}
 
-		private void listTable() throws VeicoliException {
-			List<String> lT = db.listOfTable("car_db");
-			lT.forEach(t -> System.out.println(t));
-		}
-
-		private void getAllMoto() {
-			System.out.println(">>>getAllMoto<<<");
-			
-			try {
-				List<Moto> lM = daoMoto.findAll();
-				lM.forEach(d -> System.out.println(d));
-
-			} catch (Exception e) {
-				System.err.println("Error found " + e.getMessage());
-			}
-		}
-		
-		private void getMotoById(Integer id) {
-			System.out.println(">>>getMotoById<<<");
-			try {
-				Optional<Moto> moto = daoMoto.findById(id);
-				if(moto.isPresent())
-					System.out.println(moto.get());
-				else {
-					System.out.println("Moto non trovata: " + id);
-				}
-			} catch (Exception e) {
-				System.err.println("Error found " + e.getMessage());
-			}
-		}
-		
-		private int createVeicolo() throws VeicoliException {
-			System.out.println("Insert into veicoli:");
-			System.out.println(SQLConfiguration.getInstance().getQuery("update.veicoli.insert"));
-			
-			int num = 0;
-			
-			Veicoli vei = new Veicoli(null, null, null, null, null, null, null, null);
-			
-			try {
-				num = daoV.insert("update.veicoli.insert", vei);
-				System.out.println("Inserimento veicolo: " + num);
-				
-				List<Veicoli> lC = daoV.findAll();
-				lC.forEach(c -> System.out.println(c));
-				return num;
-			} catch (Exception e) {
-				System.err.println("inserimento fallito: " + num);
-			}
-			return num;
-		}
-		
-		private void createMoto(int idVeicolo) throws Exception {
-			System.out.println("insert into Moto*****");
-			Moto moto = new Moto(idVeicolo, "TRGMot", 125);
-
-			daoMoto.insert(moto);
-			System.out.println("Moto creato");
+			db.commit();
+		} catch (Exception e) {
+			System.err.println("Error found: " + e.getMessage());
+			System.err.println("Execute rollback...");
+			db.rollback();
 		}
 	}
 
+	private void listTable() throws VeicoliException {
+		List<String> lT = db.listOfTable("car_db");
+		lT.forEach(t -> System.out.println(t));
+	}
+
+	private void getAllMoto() {
+		System.out.println(">>>getAllMoto<<<");
+
+		try {
+			List<Moto> lM = daoMoto.findAll();
+			lM.forEach(d -> System.out.println(d));
+
+		} catch (Exception e) {
+			System.err.println("Error found " + e.getMessage());
+		}
+	}
+
+	private void getMotoById(Integer id) {
+		System.out.println(">>>getMotoById<<<");
+		try {
+			Optional<Moto> moto = daoMoto.findById(id);
+			if (moto.isPresent())
+				System.out.println(moto.get());
+			else {
+				System.out.println("Moto non trovata: " + id);
+			}
+		} catch (Exception e) {
+			System.err.println("Error found " + e.getMessage());
+		}
+	}
+
+	private int createVeicolo() throws Exception {
+		System.out.println("Insert into veicoli:");
+		int num = 0;
+
+		Veicoli vei = new Veicoli(null, null, null, null, null, null, null, null);
+
+		num = daoV.insert("update.veicoli.insert", vei);
+		System.out.println("Inserimento veicolo: " + num);
+
+		Optional<Veicoli> v = daoV.findById(num);
+		if (v.isEmpty())
+			throw new VeicoliException("Veicolo non trovato");
+		System.out.println(v.get());
+
+		return num;
+	}
+
+	private void createMoto(int idVeicolo) throws Exception {
+		System.out.println("insert into Moto*****");
+		Moto moto = new Moto(idVeicolo, "TRGMot", 125);
+
+		daoMoto.insert(moto);
+		System.out.println("Moto creato");
+	}
+}
